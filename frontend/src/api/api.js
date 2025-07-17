43d52a7e91015028ce8b011d33296a3046f340ac
Prince-919 <princesharmabbu0@gmail.com>
2025-07-18 01:04:31 +0530

    build the project

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const localURL = "http://localhost:5000";
-const productionURL = "";
+const productionURL = "https://banavo.vercel.app";
 
 const token = localStorage.getItem("canva_token");
 
 const api = axios.create({
-  baseURL: localURL,
+  baseURL: productionURL,
   headers: {
     Authorization: token ? `Bearer ${token}` : "",
   },
